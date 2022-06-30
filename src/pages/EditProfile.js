483df7b140e599eb1976cf86dfbd1480cefe2b32
jpcarreon <jccarreon3@up.edu.ph>
2022-06-30 16:10:28 +0800

    fix: missing header import

--- src/pages/EditProfile.js
+++ src/pages/EditProfile.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 
+import Header from "../components/Header";
+
 export default class Home extends Component {
     constructor(props) {
         super(props);
